3c5b0ce0a50b14182a72347025fa3e1ca1519b5b
elihart <konakid@gmail.com>
2013-10-24 15:37:12 -1000

    Create rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LyricMatcher::Application.config.secret_key_base = '5b9a9ad59b29fa50b090090c1698cce687c27e0cf53c34b81454d905ee03c87ff057979711f057ed9dd008d8583f1fc92f833ae313c23ccfe83bf7523b40fd9c'
